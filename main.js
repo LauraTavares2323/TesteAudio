a8304fb65832faf9c940cfd439dbf90033bf5234
Laura Tavares <144948480+LauraTavares2323@users.noreply.github.com>
2025-04-04 23:03:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const playBtn = document.getElementById('play'),
+  resetBtn = document.getElementById('reset'),
+  hearbeat = document.getElementById('heartbeat')
+  audios = document.querySelectorAll('audio');
+console.log(audios);
+ 
+ 
+playBtn.addEventListener('mouseover', function() {
+[].forEach.call(audios, function(audio) {
+  audio.play();
+});
+}, false);
+ 
+playBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
+ 
+resetBtn.addEventListener('mouseover', function() {
+    heartbeat.play();
+}, false);
+ 
+resetBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
